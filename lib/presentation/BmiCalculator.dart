b933fd6ca2d8e1003640db1bb50e17166cece198
luca-hom <luca.homberger@juventus.schule>
2022-12-11 14:44:52 +0100

    Added Padding to welcome text, created seperated, external appbar

--- lib/presentation/BmiCalculator.dart
+++ lib/presentation/BmiCalculator.dart
@@ -1,22 +1,18 @@
 import 'package:flutter/material.dart';
 
+import 'BaseAppBar.dart';
+
 class BmiCalculator extends StatelessWidget {
   const BmiCalculator ({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: const IconButton(
-          icon: Icon(Icons.menu),
-          tooltip: 'Navigation menu',
-          onPressed: null,
+        appBar: BaseAppBar(
+          title: Text('BMI Calculator'),
+          appBar: AppBar(),
+          widgets: <Widget>[Icon(Icons.more_vert)],
         ),
-        title: const Text('BMI Calculator'),
-        actions: const [
-        ],
-      ),
-    );
+        body: Container());
   }
-
 }
--- lib/presentation/WelcomePage.dart
+++ lib/presentation/WelcomePage.dart
@@ -1,31 +1,29 @@
 import 'package:bmi/presentation/BmiCalculator.dart';
 import 'package:flutter/material.dart';
 
+import 'BaseAppBar.dart';
+
 class WelcomePage extends StatelessWidget {
   const WelcomePage ({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: const IconButton(
-          icon: Icon(Icons.menu),
-          tooltip: 'Navigation menu',
-          onPressed: null,
-        ),
-        title: const Text('Welcome Page'),
-        actions: const [
-        ],
+      appBar: BaseAppBar(
+        title: Text('Welcome Page'),
+        appBar: AppBar(),
+        widgets: <Widget>[Icon(Icons.more_vert)],
       ),
       // body is the majority of the screen.
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text('Body mass index (BMI) is a value derived from the mass (weight) '
-              'and height of a person. The BMI is defined as the body mass divided by '
-              'the square of the body height, and is expressed in units of kg/m2, '
-              'resulting from mass in kilograms and height in metres.'),
+          Padding(padding: EdgeInsets.fromLTRB(20, 0, 20, 20),
+                  child: Text('Body mass index (BMI) is a value derived from the mass (weight) '
+                      'and height of a person. The BMI is defined as the body mass divided by '
+                      'the square of the body height, and is expressed in units of kg/m2, '
+                      'resulting from mass in kilograms and height in metres.')),
         TextButton(
           onPressed: () {
             Navigator.push(
