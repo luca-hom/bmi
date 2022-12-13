50832a666eb9da9e75bd86e7b5db274f8fa58c91
luca-hom <luca.homberger@juventus.schule>
2022-12-13 13:56:46 +0100

    Added Bmi Calc Page

--- lib/presentation/BmiCalculator.dart
+++ lib/presentation/BmiCalculator.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 import 'BaseAppBar.dart';
@@ -12,7 +13,51 @@ class BmiCalculator extends StatelessWidget {
           title: Text('BMI Calculator'),
           appBar: AppBar(),
         ),
-        body: Container(
+        body: Column(
+          children: [
+            Padding(padding: const EdgeInsets.fromLTRB(20, 50, 20, 20),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Enter Weight (kg)'
+                    ),
+                    keyboardType: TextInputType.number,
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Enter Height (cm)'
+                    ),
+                    keyboardType: TextInputType.number,
+                  )
+                ]
+              ),
+            ),
+            Expanded(child: Align(
+              alignment: FractionalOffset.bottomCenter,
+              child: Padding(
+                padding: EdgeInsets.all(15.0),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () => {},
+                        child: Text('Clear Input'),
+                      ),
+                      ElevatedButton(
+                        onPressed: () => {},
+                        child: Text('Calculate BMI'),
+                      )
+                    ]
+                ),
+              )
+
+
+            ))
+
+          ],
 
         ));
   }
--- lib/presentation/WelcomePage.dart
+++ lib/presentation/WelcomePage.dart
@@ -23,7 +23,7 @@ class WelcomePage extends StatelessWidget {
                       'and height of a person. The BMI is defined as the body mass divided by '
                       'the square of the body height, and is expressed in units of kg/m2, '
                       'resulting from mass in kilograms and height in metres.')),
-        TextButton(
+        ElevatedButton(
           onPressed: () {
             Navigator.push(
                 context,
