974e5a12e417a7142eddb8885b3d76de076e6d7c
luca-hom <luca.homberger@juventus.schule>
2022-12-18 12:16:13 +0100

    added BMI-Result Screen with basic functionality

--- lib/application/BmiUtil.dart
+++ lib/application/BmiUtil.dart
@@ -1,4 +1,6 @@
+import 'dart:core';
 import 'dart:math';
+
 
 class BmiUtil {
 
@@ -19,5 +21,48 @@ class BmiUtil {
     return weight / pow(height / 100, 2);
   }
 
+  Rating getRating() {
+    var bmi = getBMI();
+    if (bmi < Rating.UNDERWEIGHT_VERY_SEVERELY.high) {
+      return Rating.UNDERWEIGHT_VERY_SEVERELY;
+    } else if (bmi < Rating.UNDERWEIGHT_SEVERELY.high) {
+      return Rating.UNDERWEIGHT_SEVERELY;
+    } else if (bmi < Rating.UNDERWEIGHT_MODERATELY.high) {
+      return Rating.UNDERWEIGHT_MODERATELY;
+    } else if (bmi < Rating.UNDERWEIGHT_SLIGHTLY.high) {
+      return Rating.UNDERWEIGHT_SLIGHTLY;
+    } else if (bmi < Rating.NORMAL.high) {
+      return Rating.NORMAL;
+    } else if (bmi < Rating.OVERWEIGHT.high) {
+      return Rating.OVERWEIGHT;
+    } else if (bmi < Rating.OBESE_MODERATELY.high) {
+      return Rating.OBESE_MODERATELY;
+    } else if (bmi < Rating.OBESE_SEVERELY.high) {
+      return Rating.OBESE_SEVERELY;
+    } else {
+      return Rating.OBESE_VERY_SEVERELY;
+    }
+  }
+
+}
+
+enum Rating {
+  UNDERWEIGHT_VERY_SEVERELY(double.negativeInfinity, 15.0),
+  UNDERWEIGHT_SEVERELY(15.0, 16.0),
+  UNDERWEIGHT_MODERATELY(16.0, 17.0),
+  UNDERWEIGHT_SLIGHTLY(17.0, 18.5),
+  NORMAL(18.5, 25.0),
+  OVERWEIGHT(25.0, 30.0),
+  OBESE_MODERATELY(30.0, 35.0),
+  OBESE_SEVERELY(35.0, 40.0),
+  OBESE_VERY_SEVERELY(40.0, double.infinity);
+
+  const Rating(this.low, this.high);
+  final double low;
+  final double high;
+
+   static List<Rating> getDescriptions() {
+     return Rating.values;
+   }
 
 }
--- lib/presentation/BmiResult.dart
+++ lib/presentation/BmiResult.dart
@@ -1,6 +1,9 @@
 
 import 'package:bmi/application/BmiUtil.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'BaseAppBar.dart';
 
 class BmiResult extends StatelessWidget {
   const BmiResult({super.key, required this.weight, required this.height});
@@ -18,8 +21,59 @@ class BmiResult extends StatelessWidget {
 
     debugPrint('result: $result');
 
-    // TODO: implement build
-    throw UnimplementedError();
+    var resultRating = BmiUtil.bmiUtilString(height, weight).getRating();
+    debugPrint('resultRating: $resultRating');
+
+
+    return Scaffold(
+      appBar: BaseAppBar(
+        title: Text('BMI Result'),
+        appBar: AppBar(),
+      ),
+      body: Column(
+        children: [
+          Padding(padding: const EdgeInsets.fromLTRB(20, 50, 20, 20),
+            child: Column(
+              children: [
+                TextFormField(
+                  readOnly: true,
+                  initialValue: weight,
+                  decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      labelText: 'Weight (kg)',
+                  ),
+                ),
+                TextFormField(
+                  readOnly: true,
+                  initialValue: height,
+                  decoration: const InputDecoration(
+                    border: UnderlineInputBorder(),
+                    labelText: 'Height (cm)',
+                  ),
+                ),
+                TextFormField(
+                  readOnly: true,
+                  initialValue: result.toStringAsFixed(2),
+                  decoration: const InputDecoration(
+                    border: UnderlineInputBorder(),
+                    labelText: 'BMI Result',
+                  ),
+                ),
+                TextFormField(
+                  readOnly: true,
+                  initialValue: resultRating.toString() ,
+                  decoration: const InputDecoration(
+                    border: UnderlineInputBorder(),
+                    labelText: 'BMI Rating',
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+
   }
 
 }
